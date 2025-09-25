cf24c537e7fc1afe44abfa122d717504aeb2c65a
tatarysh <tatarysh@gmail.com>
2025-09-25 16:33:04 +0200

    feat: Added ~/.ssh/id_rsa generator

--- src/generators/index.ts
+++ src/generators/index.ts
@@ -2,12 +2,14 @@
 import envGenerator from "./env-generator";
 import gitConfigGenerator from "./git-config-generator";
 import wpConfigGenerator from "./wp-config-generator";
+import sshKeyGenerator from "./ssh-key-generator";
 import { Generator } from "../types/index";
 
 const generators: Generator[] = [
   envGenerator,
   gitConfigGenerator,
   wpConfigGenerator,
+  sshKeyGenerator,
   // Future generators will be added here
 ];
 
---
+++ src/generators/ssh-key-generator.ts
@@ -0,0 +1,28 @@
+
+import { ChaffResponse, Generator } from "../types/index";
+import { PRNG, simpleHash } from "../utils/prng-utils";
+
+const generateSshKeyContent = (prng: PRNG): string => {
+  const keyLength = prng.nextInt(2000, 3000); // Simulate variable key length
+  const keyContent = prng.nextString(keyLength);
+  const email = `${prng.nextString(prng.nextInt(5, 8))}@${prng.nextString(prng.nextInt(4, 7))}.com`;
+  const date = new Date(prng.nextInt(2010, 2023), prng.nextInt(0, 11), prng.nextInt(1, 28)).toISOString().split('T')[0];
+
+  return `-----BEGIN RSA PRIVATE KEY-----
+${keyContent.match(/.{1,64}/g)?.join('\n') || keyContent}
+-----END RSA PRIVATE KEY-----
+${email} ${date}
+`;
+};
+
+const sshKeyGenerator: Generator = {
+  pattern: /(^|\/)\.ssh\/id_rsa$/,
+  generator: (path: string): ChaffResponse => {
+    const seed = simpleHash(path);
+    const prng = new PRNG(seed);
+    const sshKeyContent = generateSshKeyContent(prng);
+    return { content: sshKeyContent, type: 'text' };
+  },
+};
+
+export default sshKeyGenerator;
